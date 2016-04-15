d8b50d3061387ff162bf9ae80c4b7f0926e0dc77
David Staudt <dstaudt@cisco.com>
2016-04-15 01:44:33 -0500

    - add /isUser resource - factor response to json - log location working dir fix

--- roombot.py
+++ roombot.py
@@ -1,5 +1,4 @@
-from flask import Flask
-from flask import request
+from flask import Flask, request
 import requests
 import json
 import configparser
@@ -15,7 +14,35 @@ import os
 application = Flask(__name__)
 CORS(application)
 
-@application.route('/adduser')
+@application.route('/isUser', methods=['GET'])
+def isUser():
+    userId = request.args['user']
+
+    header = {'Authorization': 'Bearer '+accessToken }
+
+    resp = requests.get('https://api.ciscospark.com/v1/people?email='+userId, headers=header )
+
+    respCode = resp.status_code
+
+    if respCode == 200:
+        respString = 'Success'
+        found =  len( resp.json()['items'] ) > 0
+        log.info('isUser: user='+userId+' Success')
+    elif respCode == 400:
+        respString = 'Invalid email'
+        log.warn('isUser: user='+userId+' Invalid email')
+    elif respCode == 401:
+        respString = 'Unauthorized'
+        log.error('isUser: GET failed unauthorized')
+    else:
+        respString = 'Unexpected response'
+        log.warn('isUser returned Unexpected response')
+
+    payload = json.dumps( {'status': respString, 'return': found} )
+
+    return (payload,respCode,)
+
+@application.route('/addUser', methods=['POST'])
 def addUser():
     roomId = request.args['room']
     userId = request.args['user']
@@ -24,52 +51,48 @@ def addUser():
 
     if not validate_email(userId):
         log.warn('Invalid email address')
-        return ("Invalid email address", 400,)
+        return ( '{ "status": "Invalid email address"}', 400,)
 
     if userBlacklisted(userId):
         log.warn('User is blacklisted')
-        return ("User is blacklisted", 403,)
+        return ( '{ "status": "User is blacklisted"}', 403,)
 
     if not roomWhitelisted(roomId):
         log.warn('Room is not whitelisted')
-        return ("Room is not whitelisted", 403,)
+        return ( '{"status": "Room is not whitelisted"}', 403,)
 
-    header = {}
-    header["Content-Type"] = "application/json"
-    header['Authorization'] = "Bearer "+accessToken
+    header = { 'Content-Type': 'application/json', 'Authorization': 'Bearer '+accessToken }
+    body = { 'roomId': roomId, 'personEmail': userId }
 
-    payload = {}
-    payload['roomId'] = roomId
-    payload['personEmail'] = userId
-
-    resp = requests.post("https://api.ciscospark.com/v1/memberships", headers=header, data=json.dumps(payload) )
+    resp = requests.post('https://api.ciscospark.com/v1/memberships', headers=header, data=json.dumps(body) )
 
     respCode = resp.status_code
 
     if respCode == 200:
-        respString = "Success"
-        log.info("POST /memberships succeeded")
+        respString = 'Success'
+        log.info('POST /memberships succeeded')
     elif respCode == 400:
-        respString = "Invalid request"
+        respString = 'Invalid request'
         log.warn('POST /memberships returned Invalid request')
     elif respCode == 401:
-        respString = "Unauthorized"
-        log.warn('POST /memberships returned Unauthorized')
-    elif respCode == 403:
-        respString = "User cannot be added to the room"
-        log.warn('POST /memberships returned User cannot be added to the room')
+        respString = 'Unauthorized'
+        log.error('POST /memberships returned Unauthorized')
     elif respCode == 404:
-        respString = "Room does not exist"
+        respString = 'Room does not exist'
         log.warn('POST /memberships returned Room does not exist')
+    elif respCode == 409:
+        respString = 'User already member of requested room'
+        log.warn('POST /memberships returned User already member of requested room')
     else:
-        respString = "Unexpected response"
+        respString = 'Unexpected response'
         log.warn('POST /memberships returned Unexpected response')
 
-    return (respString, respCode, )
+    payload = json.dumps( {'status': respString} )
+    return (payload, respCode, )
 
 def userBlacklisted(userId):
     blacklisted = False
-    with open(curDir+'/user_blacklist.txt', 'r') as inFile:
+    with open( os.path.join(curDir, 'user_blacklist.txt'), 'r' ) as inFile:
         for line in inFile:
             if (userId in line) and (line[0] != '#'):
                 blacklisted = True
@@ -77,7 +100,7 @@ def userBlacklisted(userId):
 
 def roomWhitelisted(roomId):
     whitelisted = False
-    with open(curDir+'/room_whitelist.txt','r') as inFile:
+    with open( os.path.join(curDir,'room_whitelist.txt'), 'r' ) as inFile:
         for line in inFile:
             if (roomId in line) and (line[0] != '#'):
                 whitelisted = True
@@ -85,20 +108,19 @@ def roomWhitelisted(roomId):
 
 def refreshTokens():
     global accessToken
-    accessToken = ""
+    accessToken = ''
     config = configparser.ConfigParser()
-    config.read(curDir+'/secrets.ini')
+    config.read( os.path.join(curDir,'secrets.ini') )
     refreshToken = config.get('User', 'refreshToken')
     clientId = config.get('Spark App Secrets', 'clientId')
     clientSecret = config.get('Spark App Secrets', 'clientSecret')
 
-    payload = {}
-    payload['grant_type'] = "refresh_token"
-    payload['client_id'] = clientId
-    payload['client_secret'] = clientSecret
-    payload['refresh_token'] = refreshToken
+    payload = { 'grant_type': 'refresh_token',
+                'client_id': clientId,
+                'client_secret': clientSecret,
+                'refresh_token': refreshToken }
 
-    header = {"Content-Type": "application/json"}
+    header = {'Content-Type': 'application/json'}
 
     resp = requests.post('https://api.ciscospark.com/v1/access_token', headers=header, data=json.dumps(payload) )
     if resp.status_code != 200:
@@ -112,16 +134,14 @@ def refreshTokens():
 
     Timer(tokenExpires-(60*60), refreshTokens).start()
 
-global curDir
 curDir = os.path.dirname(__file__)
-#curDir  = "/var/www/html/roombot"
-global log
 log = logging.getLogger('rotating log')
 log.setLevel(logging.INFO)
-handler = RotatingFileHandler(curDir + '/roombot.log', maxBytes=1048576, backupCount = 10)
+handler = RotatingFileHandler(os.path.join(curDir, 'roombot.log'), maxBytes=1048576, backupCount = 10)
 handler.setFormatter(Formatter('%(asctime)s:%(levelname)s:%(message)s') )
 log.addHandler(handler)
+
 refreshTokens()
 
 if __name__ == '__main__':
-    application.run(host='0.0.0.0')
+    application.run(host='0.0.0.0',debug=True)
